38be03db425094870b3877dcab8660f2b8ecfdca
Mihai <mihaipaulx@gmail.com>
2024-03-12 22:23:51 +0200

    ec2

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -46,6 +49,10 @@ def get_overall(uni_url):
     url = link.build(query)
 
     driver.get(url)
+
+    # Wait for the page to load
+    wait = WebDriverWait(driver, 10)  # Adjust timeout as needed
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
     accept_cookies(driver)
 
