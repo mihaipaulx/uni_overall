c34abd37dac7f9be6ff5eb9b207f4fa39863adb0
Mihai <mihaipaulx@gmail.com>
2024-03-12 20:50:05 +0200

    ec2

--- script.py
+++ script.py
@@ -25,6 +25,8 @@ MAX_SLEEP = int(os.getenv('MAX_SLEEP'))
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox") 
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--blink-settings=imagesEnabled=false")
 chrome_options.add_argument("--log-level=3")
